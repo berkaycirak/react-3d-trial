d056393b7f326393b99cd7aecb5d16b7bd327123
Berkay ÇIRAK <berkaycirak_11@hotmail.com>
2023-01-04 19:13:58 +0300

    Video Screen is added

---
+++ models/Video.jsx
@@ -0,0 +1,33 @@
+import { useAspect } from '@react-three/drei';
+import { useEffect, useState } from 'react';
+import * as THREE from 'three';
+
+function Video() {
+	const scale = useAspect(1920, 1080, 1);
+
+	const [video] = useState(() =>
+		Object.assign(document.createElement('video'), {
+			src: '/planet.mp4',
+			crossOrigin: 'Anonymous',
+			loop: true,
+			muted: true,
+		})
+	);
+
+	useEffect(() => void video.play(), [video]);
+
+	return (
+		<mesh scale={scale}>
+			<planeGeometry />
+			<meshBasicMaterial side={THREE.DoubleSide}>
+				<videoTexture
+					attach='map'
+					args={[video]}
+					encoding={THREE.sRGBEncoding}
+				/>
+			</meshBasicMaterial>
+		</mesh>
+	);
+}
+
+export default Video;
--- overlay/SceneContainer.jsx
+++ overlay/SceneContainer.jsx
@@ -10,20 +10,22 @@ import Portal from '../models/Portal';
 import Rocks from '../models/Rocks';
 import Trees from '../models/Trees';
 import NFTCarousel from '../models/NFTCarousel';
+import Video from '../models/Video';
 
 function SceneContainer() {
 	return (
 		<Suspense>
 			<ambientLight color={'#ffffff'} intensity={1} />
 			{/* Camera */}
-			<PerspectiveCamera fov={35} makeDefault position={[5, 0, 10]} />
+			<PerspectiveCamera fov={35} makeDefault position={[5, 0, 20]} />
 
-			{/* <OrbitControls
-				target={[1, 7, 0]}
+			<OrbitControls
+				target={[1, 0, 0]}
 				maxPolarAngle={Math.PI * 0.5}
-			/> */}
+			/>
+			<Video />
 
-			<NFTCarousel />
+			{/* <NFTCarousel /> */}
 			<Environment background>
 				<mesh scale={100}>
 					<sphereGeometry args={[1, 64, 64]} />
